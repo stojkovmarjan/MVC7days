90ef56d9501843b985da52e6549c0a21ca031be0
Maryan Stoykov <stojkovmarjan@gmail.com>
2017-07-19 22:22:07 +0200

    Return object

--- MVC7days/Controllers/TestController.cs
+++ MVC7days/Controllers/TestController.cs
@@ -6,6 +6,17 @@ using System.Web.Mvc;
 
 namespace MVC7days.Controllers
 {
+
+    public class Customer
+    {
+        public string CustomerName { get; set; }
+        public string Address { get; set; }
+        public override string ToString()
+        {
+            return this.CustomerName + "|" + this.Address;
+        }
+    }
+
     public class TestController : Controller
     {
         /* GET: Test
@@ -13,12 +24,26 @@ namespace MVC7days.Controllers
         {
             return View();
         }
-        */
+       
 
         public string GetString()
         {
             return "Hallo World is old now. It's time for wassup bro";
         }
+         */
+
+       public Customer GetCustomer()
+        {
+            Customer c = new Customer
+            {
+                CustomerName = "rtetertret",
+                Address = "sdsdsdsd"
+            };
+
+            return c;
+        }
+
         
+
     }
 }
