c155eafa642fa13e445fad7fe412a03289f3a3fc
Maryan Stoykov <stojkovmarjan@gmail.com>
2017-07-19 21:55:03 +0200

    Add project files.

---
+++ MVC7days/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MVC7days
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
