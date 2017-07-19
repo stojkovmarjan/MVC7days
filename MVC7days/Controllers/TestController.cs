2be883ae67a5014dc4c91132e4122d604e911de6
Maryan Stoykov <stojkovmarjan@gmail.com>
2017-07-19 22:10:54 +0200

    Lab1_1

---
+++ MVC7days/Controllers/TestController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC7days.Controllers
+{
+    public class TestController : Controller
+    {
+        /* GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+        */
+
+        public string GetString()
+        {
+            return "Hallo World is old now. It's time for wassup bro";
+        }
+        
+    }
+}
