200284f8a122d8fa3454bd0376806c99aadbfda6
Maryan Stoykov <stojkovmarjan@gmail.com>
2017-07-20 07:27:52 +0200

    GetView()

--- MVC7days/Controllers/TestController.cs
+++ MVC7days/Controllers/TestController.cs
@@ -6,19 +6,35 @@ using System.Web.Mvc;
 
 namespace MVC7days.Controllers
 {
+    public class Customer
+    {
+        public string CustomerName { get; set; }
+        public string Address { get; set; }
+        public override string ToString()
+        {
+            return this.CustomerName + "|" + this.Address;
+        }
+    }
+
     public class TestController : Controller
     {
-        /* GET: Test
-        public ActionResult Index()
-        {
-            return View();
-        }
-        */
-
-        public string GetString()
-        {
-            return "Hallo World is old now. It's time for wassup bro";
-        }
         
+        public Customer GetCustomer()
+        {
+            Customer c = new Customer
+            {
+                CustomerName = "rtetertret",
+                Address = "sdsdsdsd"
+            };
+
+            return c;
+        }
+
+        public ActionResult GetView()
+        {
+            return View("MyView");
+        }
+
     }
+
 }
